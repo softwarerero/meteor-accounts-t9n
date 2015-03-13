0f54e84321b920084f678e3ca7bac6bd45c778af
bySabi Files <flxinformatico@gmail.com>
2015-03-13 08:20:40 +0100

    WIP

---
+++ locales/de/package.js
@@ -0,0 +1,16 @@
+Package.describe({
+    name: 'softwarerero:accounts-locale-de',
+    summary: 'softwarerero:accounts de locale. Companion package for softwarerero:accounts',
+    version: '2.0.0',
+    git: 'https://github.com/softwarerero/meteor-accounts-t9n.git'
+});
+
+Package.on_use(function (api, where) {
+    if (api.versionsFrom)
+        api.versionsFrom("METEOR@0.9.0");
+
+    api.add_files('locale.coffee');
+
+    api.use('softwarerero:accounts@2.0.0');
+    api.imply('softwarerero:accounts');
+});
