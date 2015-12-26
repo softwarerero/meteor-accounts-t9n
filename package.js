b1209c693ea382f62084d4770329931b71d6554c
softwarerero <el@softwarerero.com>
2015-12-26 20:19:17 -0300

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Almost i18n, with standard translations for basic meteor packages.",
-  version: "1.1.7",
+  version: "1.2",
   name: "softwarerero:accounts-t9n",
   git: "https://github.com/softwarerero/meteor-accounts-t9n.git",
 });
