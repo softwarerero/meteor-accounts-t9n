fab1763ab2737b12dc1ec695f18cfebbabd6bdba
softwarerero <el@softwarerero.com>
2017-08-30 21:49:27 -0400

    Add back reactivity

--- examples/meteorNpm/client/index.js
+++ examples/meteorNpm/client/index.js
@@ -6,6 +6,10 @@ T9n.map('de', {
   'hello': 'Hallo @{name}!',
   'pun': '@{subject} @{predicate} @{adverb} @{object}. Frische @{object} @{predicate} @{subject}.',
 });
+T9n.map('es', {
+  'hello': 'Hola @{name}!',
+  'pun': 'No anda bien en castellano eso de "@{subject} @{predicate} @{adverb} @{object}. Frische @{object} @{predicate} @{subject}".',
+});
 T9n.setLanguage('de');
 
 // as the template helper is not registered at this moment do it here
@@ -13,5 +17,7 @@ Template.registerHelper('t9n', (x, params) => T9n.get(x, true, params.hash));
 Template.registerHelper('currentLang', () => T9n.getLanguage());
 
 Template.app.events({
-    'click button': () => T9n.setLanguage('es'), // no reactivity!
+    'click button': () => T9n.setLanguage(flipLanguage()),
 });
+
+const flipLanguage = () => T9n.getLanguage() === 'es' ? 'de' : 'es';
